764ec902fb18a56ae59f2564d919202361431357
Iure Silva <40864373+iuresg@users.noreply.github.com>
2022-10-04 12:16:55 -0300

    add new function

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ console.log(result)
 function multiply(a,b) {
   return  a * b
 }
+
+const res = multiply(6,8)
+
+console.log(res)
+
+function division(a, b){
+   return a / b
+}
+
+const resul = division(9, 4)
